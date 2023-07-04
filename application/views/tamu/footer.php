072ae27ff81c04dcd2e25d4274e8b74187184a3e
Dimassaputra11 <adimasbudisaputra4@gmail.com>
2023-07-05 03:39:08 +0700

    first commit

---
+++ application/views/tamu/footer.php
@@ -0,0 +1,8 @@
+<!-- Sticky Footer -->
+<footer >
+  <div class="container mx-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright © TheLucky Hotel 2023</span>
+    </div>
+  </div>
+</footer>
